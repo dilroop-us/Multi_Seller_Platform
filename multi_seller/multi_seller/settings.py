2f1198d17a027b970be14ab38fbf396d83dc66d9
Dilroop U S <65127204+dilroop-us@users.noreply.github.com>
2024-12-16 20:13:35 +0530

    end of the first day

--- multi_seller/multi_seller/settings.py
+++ multi_seller/multi_seller/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'multi_seller.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'multi_seller',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
